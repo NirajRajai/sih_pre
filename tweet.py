6e2a5125e9772a230a3faa85f905043a0c650764
CP-4 <chinmaypatel02@gmail.com>
2018-03-14 04:14:25 +0530

    writting tweets to file. BUG:crashed on encountering UTF-16

--- tweet.py
+++ tweet.py
@@ -1,4 +1,5 @@
 from birdy.twitter import StreamClient, UserClient
+import io
 
 ACCESS_TOKEN = '969576094407057408-UPSzwvMLEcYNIe2nAewlX56vIdYUtyl'
 ACCESS_SECRET = 'Rq5fDXOF6AMKZx7nLJo7XFo8xFN1lLPlTqnaDLXlP1hcK'
@@ -7,7 +8,12 @@ CONSUMER_SECRET = 'WzKU46BZloHztcHzRkUfRZaJ5Pqt6VSsAucQ4eJBsWGZalKDiu'
 
 client = StreamClient(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 
-resource = client.stream.statuses.filter.post(track='RailMinIndia')
+resource = client.stream.statuses.filter.post(track='India')
 
-for data in resource.stream():
-    print (data.text)
+with open("tweet.txt", "a") as file:
+    for data in resource.stream():
+        if 'RT @' not in data.text:
+            print (data.text+"\n\n\n")
+            file.write(data.text)
+    
+
